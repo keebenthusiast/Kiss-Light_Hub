e8af9f15c4c2d8b3b05c028d5853acc6381dc066
Christian K <nxtpuny@gmail.com>
2021-01-23 07:32:20 -0800

    Add column which keeps track of device state.

--- resources/server-db.sql
+++ resources/server-db.sql
@@ -34,6 +34,7 @@ CREATE TABLE device (
     dev_name VARCHAR NOT NULL,
     mqtt_topic VARCHAR NOT NULL,
     dev_type INT NOT NULL,
+    dev_state INT NOT NULL, -- zero or one
     PRIMARY KEY( dev_name )
 );
 
